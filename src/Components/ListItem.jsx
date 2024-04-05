29c307b9f0a506c3d060e5012b923afa6dd334c6
Rui Folgado <rui.folgado@hotmail.com>
2024-04-05 16:41:52 +0100

    Edit Function Form

--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -6,35 +6,45 @@ import { MdRadioButtonUnchecked } from "react-icons/md";
 import { LuCheckCircle } from "react-icons/lu";
 import { GrEdit } from "react-icons/gr";
 import Edit from '../assets/Pages/EditPage';
-
+;
 const ListItem = ({ currentTask, remove }) => {
-    const [editable, setEditable]= useState(false)
+    const [editable, setEditable] = useState(false);
+    const [editedTask, setEditedTask] = useState(currentTask.task);
+
+    const handleEdit = () => {
+        setEditable(true);
+    };
+
+    const handleInputChange = (e) => {
+        setEditedTask(e.target.value);
+    };
+
+    const handleSave = () => {
+        // Save changes
+        setEditable(false);
+        // You can send the edited task to a parent component to update the tasks array
+        
+    };
 
     return (
-        
-
         <li className='item'>
-            <div  style={{ display: 'flex', gap: "10px" }}> <div>{currentTask.completed ? <LuCheckCircle size={30} /> : <MdRadioButtonUnchecked size={32} />}</div>
-                <Link to={`/task-details/${currentTask.id}`} className={currentTask.completed ? "crossedOut" : ""}>{currentTask.task}</Link>
+            <div style={{ display: 'flex', gap: "10px" }}>
+                <div>{currentTask.completed ? <LuCheckCircle size={30} /> : <MdRadioButtonUnchecked size={32} />}</div>
+                {editable ? (
+                    <input type="text" value={editedTask} onChange={handleInputChange} autoFocus />
+                ) : (
+                    <Link to={`/task-details/${currentTask.id}`} className={currentTask.completed ? "crossedOut" : ""}>{currentTask.task}</Link>
+                )}
             </div>
             <div className='icons'>
-            <div>
-
-
-            <Link to="/edit">
-             <GrEdit style={{ cursor: "pointer" }} size={24}  />
-            </Link>
-                
-                
-                   
+                <div>
+                    <GrEdit style={{ cursor: "pointer" }} size={24} onClick={handleEdit} />
+                </div>
+                <div>
+                    <MdDeleteOutline style={{ cursor: "pointer" }} size={24} onClick={() => remove(currentTask)} />
+                </div>
             </div>
-            <div>
-                <MdDeleteOutline style={{ cursor: "pointer" }} size={24} onClick={() => remove(currentTask)} />
-            </div>
-            </div>
-            
         </li>
-
     );
 };
 export default ListItem;
