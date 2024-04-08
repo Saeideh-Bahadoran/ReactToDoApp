eee77383f2a29cf44101b2a3f1585919d0e99bf9
Rui Folgado <rui.folgado@hotmail.com>
2024-04-08 15:13:18 +0100

    Implemented completed / not completed icons

--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom";
 import { MdDeleteOutline, MdRadioButtonUnchecked } from "react-icons/md";
 import { LuCheckCircle } from "react-icons/lu";
 import { GrEdit } from "react-icons/gr";
-
 import { MdOutlineFileDownloadDone, MdCancel } from "react-icons/md";
+
 const ListItem = ({ currentTask, remove }) => {
     const [editable, setEditable] = useState(false);
     const [editedTask, setEditedTask] = useState(currentTask.task);
+    const [completed, setCompleted] = useState(currentTask.completed); //Initial complete status
+
+    const handleToggleCompletion = () => {
+        setCompleted(!completed); // Toggle Completed or not completed 
+    };
 
     const handleEdit = () => {
         setEditable(true);
@@ -29,40 +34,40 @@ const ListItem = ({ currentTask, remove }) => {
     return (
         <li className='item'>
             <div style={{ display: 'flex', gap: "10px" }}>
-                <div>{currentTask.completed ? <LuCheckCircle size={30} /> : <MdRadioButtonUnchecked size={32} />}</div>
+                <div onClick={handleToggleCompletion}>
+                    {completed ? <LuCheckCircle size={30} /> : <MdRadioButtonUnchecked size={32} />}
+                </div>
                 {editable ? (
                     <>
-                    <input type="text" value={editedTask} onChange={handleInputChange} autoFocus />
-                    <MdOutlineFileDownloadDone  style={{ cursor: "pointer", marginRight:"10px" , color:"green"}} size={24} onClick={handleSave} />
-                    <MdCancel style={{ cursor: "pointer", marginRight:"10px", color:"tomato" }} size={24} onClick={() => {setEditable(false)
-                         setEditedTask(currentTask.task)}} /> 
-                         </>
+                        <input type="text" value={editedTask} onChange={handleInputChange} autoFocus />
+                        <MdOutlineFileDownloadDone style={{ cursor: "pointer", marginRight: "10px", color: "green" }} size={24} onClick={handleSave} />
+                        <MdCancel style={{ cursor: "pointer", marginRight: "10px", color: "tomato" }} size={24} onClick={() => { setEditable(false); setEditedTask(currentTask.task) }} />
+                    </>
                 ) : (
-                    <Link to={`/task-details/${currentTask.id}`} className={currentTask.completed ? "crossedOut" : ""}>{currentTask.task}</Link>
+                    <Link to={`/task-details/${currentTask.id}`} className={completed ? "crossedOut" : ""}>{currentTask.task}</Link>
                 )}
             </div>
             <div className='icons'>
-    <div>
-        {/* {editable && (
-            <>
-               
-                <MdOutlineDataSaverOn style={{ cursor: "pointer", marginRight:"10px" }} size={24} onClick={handleSave} />
-                <MdCancel style={{ cursor: "pointer", marginRight:"10px" }} size={24} onClick={() => {setEditable(false)
-                     setEditedTask(currentTask.task)}} /> 
-            </>
-        )} */}
-        <GrEdit style={{ cursor: "pointer" }} size={24} onClick={handleEdit} />
-    </div>
-    <div>
-        <MdDeleteOutline style={{ cursor: "pointer" }} size={24} onClick={() => remove(currentTask)} />
-    </div>
-</div>
+                <div>
+                    <GrEdit style={{ cursor: "pointer" }} size={24} onClick={handleEdit} />
+                </div>
+                <div>
+                    <MdDeleteOutline style={{ cursor: "pointer" }} size={24} onClick={() => remove(currentTask)} />
+                </div>
+            </div>
         </li>
     );
 };
 
 export default ListItem;
+/*
+const [completed, setCompleted] = useState(currentTask.completed); //Initial complete status
 
+    const handleToggleCompletion = () => {
+        setCompleted(!completed); // Toggle Completed or not completed 
+    };
 
-//<MdOutlineDataSaverOn />
-//<MdCancel />
+    <div onClick={handleToggleCompletion}>{completed ? <LuCheckCircle size={30} /> : <MdRadioButtonUnchecked size={32} />}</div>
+         
+    this is the relevant code for the completed or not completed status and renders on screen icons :)
+*/ 
